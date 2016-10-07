0addcaa833543e3767923ff41fafdf0cf57e8c8e
b27676110 <105021036@live.asia.edu.tw>
2016-10-08 02:15:18 +0800

    20161008_105021036

--- src/hw/hw02_105021036.java
+++ src/hw/hw02_105021036.java
@@ -1,6 +1,6 @@
 package hw;
 import java.util.Scanner;
-public class hw02_1050210XX {
+public class hw02_105021036 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
